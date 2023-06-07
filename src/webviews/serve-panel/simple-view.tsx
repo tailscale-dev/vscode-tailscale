d2784e5630c54f4faf6070ad39d9b46d58c7de31
Marwan Sulaiman <marwan.sameer@gmail.com>
2023-06-07 11:35:59 -0400

    SimpleView: only show service message when there is a port (#9)
    Do not show an error message when a user has not yet typed in a port.
    
    ---------
    
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>
    Co-authored-by: Tyler Smalley <tyler@tailscale.com>
    (cherry picked from commit ddbe013d4383fb728b475b11f2e80f869c9189a4)

--- src/webviews/serve-panel/simple-view.tsx
+++ src/webviews/serve-panel/simple-view.tsx
@@ -164,19 +164,25 @@ export const SimpleView = () => {
             </Tooltip>
           )}
         </div>
-        <div className="pt-4">
-          {data?.Services[port] ? (
-            <div className="italic">
-              Port {port} is currently started by "{data?.Services[port]}"
-            </div>
-          ) : (
-            <div className="text-errorForeground">
-              It seems there's no service currently utilizing port {port}. Please ensure you start a
-              local service that is bound to port {port}.
-            </div>
-          )}
-        </div>
+        <div className="pt-4">{persistedPort && port === persistedPort ? renderService() : ''}</div>
       </form>
+    );
+  }
+
+  function renderService(): JSX.Element {
+    if (data?.Services[persistedPort]) {
+      return (
+        <div className="italic">
+          Port {persistedPort} is currently started by "{data?.Services[persistedPort]}"
+        </div>
+      );
+    }
+
+    return (
+      <div className="text-errorForeground">
+        It seems there's no service currently utilizing port {persistedPort}. Please ensure you
+        start a local service that is bound to port {persistedPort}.
+      </div>
     );
   }
 
