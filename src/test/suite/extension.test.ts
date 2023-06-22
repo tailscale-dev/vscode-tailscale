a7d98e5533cce22058041916fe17689b9710e8f8
Tyler Smalley <tyler@tailscale.com>
2023-06-22 13:45:06 -0700

    wip
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,16 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
+
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Sample test', () => {
+  test('Sample test', async () => {
+    await vscode.commands.executeCommand('tailscale-serve-view.focus');
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    // sleep for 5 seconds
+    await new Promise((resolve) => setTimeout(resolve, 5000));
   });
 });
