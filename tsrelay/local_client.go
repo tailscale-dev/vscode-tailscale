78ec2c43820f7f60220a8a0e0b76e809287405a8
Marwan Sulaiman <marwan.sameer@gmail.com>
2023-06-29 16:23:05 -0400

    Add ability to mock LocalClient (#90)
    This PR adds the ability to mock LocalClient responses via `-mockfile`
    so that we can test vscode <-> tsrelay communication without requiring a
    live tailscaled server.
    
    ---------
    
    Signed-off-by: Tyler Smalley <tyler@tailscale.com>
    Co-authored-by: Tyler Smalley <tyler@tailscale.com>
    (cherry picked from commit 2105a74e63ed1e34b210888c5d650adfe68bcefe)

---
+++ tsrelay/local_client.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net"
+	"os"
+	"sync"
+
+	"tailscale.com/client/tailscale"
+	"tailscale.com/ipn"
+	"tailscale.com/ipn/ipnstate"
+)
+
+// static check for local client interface implementation
+var _ localClient = (*tailscale.LocalClient)(nil)
+
+// localClient is an abstraction of tailscale.LocalClient
+type localClient interface {
+	Status(ctx context.Context) (*ipnstate.Status, error)
+	GetServeConfig(ctx context.Context) (*ipn.ServeConfig, error)
+	StatusWithoutPeers(ctx context.Context) (*ipnstate.Status, error)
+	SetServeConfig(ctx context.Context, config *ipn.ServeConfig) error
+}
+
+type profile struct {
+	Status           *ipnstate.Status
+	ServeConfig      *ipn.ServeConfig
+	MockOffline      bool
+	MockAccessDenied bool
+}
+
+// NewMockClient returns a mock localClient
+// based on the given json file. The format of the file
+// is described in the profile struct. Note that SET
+// operations update the given input in memory.
+func NewMockClient(file string) (localClient, error) {
+	bts, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	var p profile
+	return &mockClient{p: &p}, json.Unmarshal(bts, &p)
+}
+
+type mockClient struct {
+	sync.Mutex
+	p *profile
+}
+
+// GetServeConfig implements localClient.
+func (m *mockClient) GetServeConfig(ctx context.Context) (*ipn.ServeConfig, error) {
+	if m.p.MockOffline {
+		return nil, &net.OpError{Op: "dial"}
+	}
+	return m.p.ServeConfig, nil
+}
+
+// SetServeConfig implements localClient.
+func (m *mockClient) SetServeConfig(ctx context.Context, config *ipn.ServeConfig) error {
+	if m.p.MockAccessDenied {
+		return &tailscale.AccessDeniedError{}
+	}
+	m.Lock()
+	defer m.Unlock()
+	m.p.ServeConfig = config
+	return nil
+}
+
+// Status implements localClient.
+func (m *mockClient) Status(ctx context.Context) (*ipnstate.Status, error) {
+	if m.p.MockOffline || m.p.Status == nil {
+		return nil, &net.OpError{Op: "dial"}
+	}
+	return m.p.Status, nil
+}
+
+// StatusWithoutPeers implements localClient.
+func (m *mockClient) StatusWithoutPeers(ctx context.Context) (*ipnstate.Status, error) {
+	if m.p.MockOffline || m.p.Status == nil {
+		return nil, &net.OpError{Op: "dial"}
+	}
+	copy := *(m.p.Status)
+	copy.Peer = nil
+	return &copy, nil
+}
